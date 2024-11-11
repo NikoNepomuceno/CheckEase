d5a90d50428de76e650d7ea88e017fec4627f331
NikoNepomuceno <202312259@gordoncollege.edu.ph>
2024-11-11 17:33:44 +0800

    Delete src directory

--- src/views/StdntAttendanceView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>STUDENT ATTENDANCE</h1>
-</template>
-<script>
-export default {
-  name: 'StdntAttendanceView', 
-};
-</script>
--- src/views/StdntClearanceView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>STUDENT CLEARANCE</h1>
-</template>
-<script>
-export default {
-  name: 'StdntClearanceView', // Change accordingly
-};
-</script>
